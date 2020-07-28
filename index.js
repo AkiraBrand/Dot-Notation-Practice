b53c06b53c3078fc1964058f58788d70e8ac3a93
Akira <akira.brand@yahoo.com>
2020-07-28 01:13:36 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+var user1 = {
+  name: "Sodie",
+  age: 8,
+  breed: "Shih-tzu",
+  hobbies: ["Sun bathing", "Bossing people around", "Napping"],
+}
+
+var user2 = {
+  name: "Mangia",
+  age: 7,
+  breed: "Greater Swiss Mountain Dog",
+  hobbies: ["Checking counter for human snacks", "Drinking out of Sodie's water bowl"],
+}
+
+// console.log((user1.age + user2.age) / 2);
+// console.log(user1.name);
+// console.log(user2.age);
+// console.log(user1.hobbies);
+// console.log(`${user1.breed} and a ${user2.breed} - BFFs`)
+// console.log(user2.hobbies);
+// console.log(user2);
+// console.log("the dog's average age is " + (user1.age + user2.age)/2)
+// console.log(user2.name);
+// console.log(user2.hobbies.length);
+console.log(user2.name + " loves to: " + user2.hobbies[0]);
+
+// 10. Print a sentence out for each hobby that user2 has, including the name of that user. Example: "Mangia loves checking counter for human snacks"
